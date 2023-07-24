559b46f6d3e415892af57b7918d4a286ddd18c88
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2023-07-23 23:10:38 -0500

    Update note.route.js
    Adding comments

--- server/src/routes/note.route.js
+++ server/src/routes/note.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import noteController from '../controllers/note.controller';
-
+{/* This file simply defines all the routes and a call back function to be passed in whenever a request hits a specific route */ }
 export default express
 	.Router()
 	.post('/', noteController.createNote)
