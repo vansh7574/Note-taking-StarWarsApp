705ded778ab3024f27339566c3a7f935934bea2b
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2023-07-23 23:12:56 -0500

    Update note.js
    Adding comments

--- server/src/models/note.js
+++ server/src/models/note.js
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-
+{/* This file simply defines a model which specifies the type of object that will get stored in the MongoDB DataBase */ }
 const noteSchema = new Schema(
 	{
 		description: { type: String },
